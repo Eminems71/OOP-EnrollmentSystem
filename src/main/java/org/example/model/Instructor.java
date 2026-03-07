81394db20c100a7382af2a4c64ea60ca6837f53c
Redel Emmanuel D.Aguila <ememaguila@gmail.com>
2026-03-07 16:20:28 +0800

    Inheritance

--- src/main/java/org/example/model/Instructor.java
+++ src/main/java/org/example/model/Instructor.java
@@ -1,4 +1,40 @@
 package org.example.model;
 
 public class Instructor {
+
+    String InstructorID;
+    String InstructorName;
+    String CourseName;
+
+    public Instructor(String ID, String Name, String Course){
+       this.InstructorID = ID;
+       this.InstructorName = Name;
+       this.CourseName = Course;
+
+    }
+    public void setInstructorID(String ID){
+        this.InstructorID = InstructorID;
+
+    }
+    public String getInstructorID(){
+        return InstructorID;
+
+    }
+    public void setInstructorName(String Name){
+        this.InstructorName = InstructorName;
+    }
+    public String getInstructorName(){
+        return InstructorName;
+
+    }
+    public void setCourseName(String Course){
+        this.CourseName = CourseName;
+
+    }
+    public String getCourseName(){
+        return CourseName;
+
+    }
+
+
 }
