ed39ba4e45bcd3848ed62017efa2db2f5632b977
Redel Emmanuel D.Aguila <ememaguila@gmail.com>
2026-05-08 10:12:43 +0800

    Database

---
+++ src/main/java/org/example/service/Database.java
@@ -0,0 +1,13 @@
+package org.example.service;
+
+import org.example.model.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+
+    public static List<Student> students = new ArrayList<>();
+    public static List<Instructor> instructors = new ArrayList<>();
+    public static List<Courses> courses = new ArrayList<>();
+    public static List<Section> sections = new ArrayList<>();
+}
